97acc1a001d0d11864ab18c24b6117dcc796d2e5
Comingdonut <jamescastrejon12@gmail.com>
2016-03-02 09:08:39 -0700

    Adds explanation for methods and constructor

--- src/com/neumont/csc150/audio/AudioPlayer.java
+++ src/com/neumont/csc150/audio/AudioPlayer.java
@@ -8,17 +8,22 @@ public class AudioPlayer {
 	
 	private Clip clip;
 	
+	/**
+	 * Sets the song put in the parameters
+	 * */
 	public AudioPlayer(String s) {
 	    try {
 	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(s).getAbsoluteFile());
 	        clip = AudioSystem.getClip();
 	        clip.open(audioInputStream);
 	    } catch(Exception ex) {
-	        System.out.println("Error with playing sound.");
+	        System.out.println("Error code 404: Girlfriend not found.");
 	        ex.printStackTrace();
 	    }
 	}
-	
+	/**
+	 * Plays the song stored in AudioPlayer(String s)
+	 * */
 	public void play(){
 		if(clip == null){
 			return;
@@ -26,11 +31,18 @@ public class AudioPlayer {
 		stop();
 		clip.start();
 	}
+
+	/**
+	 * Stops the song stored in AudioPlayer(String s)
+	 * */
 	public void stop(){
 		if(clip.isRunning()){
 			clip.stop();
 		}
 	}
+	/**
+	 * Uses stop() to stop the song and closes it releasing its resources
+	 * */
 	public void close(){
 		stop();
 		clip.close();

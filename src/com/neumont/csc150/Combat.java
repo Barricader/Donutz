097c7544d8c4f206d35b315cf31607adfc698323
Comingdonut <jamescastrejon12@gmail.com>
2016-03-04 11:37:06 -0700

    Adds to render combat

--- src/com/neumont/csc150/Combat.java
+++ src/com/neumont/csc150/Combat.java
@@ -2,22 +2,41 @@ package com.neumont.csc150;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 import com.neumont.csc150.entity.Enemy;
+import com.neumont.csc150.entity.EnemyType;
 import com.neumont.csc150.entity.Player;
 
 public class Combat {
+//	Variables
+	private Random rand;
 	private Player p;
 	private Enemy e;
 	
 	public Combat(Player p,Enemy e){
+		rand = new Random();
 		this.p = p;
 		this.e = e;
 	}
 	
 	public void renderCombat(Graphics g){
 		g.setColor(Color.BLACK);
-	
+		int i = rand.nextInt(4) + 1;
+		if(i == 1){
+			e = new Enemy(0, 0, 7, 20, EnemyType.RANGED, 5, 3);
+		}
+		else if(i == 2){
+			e = new Enemy(0, 0, 8, 25, EnemyType.MELEE, 6, 5);
+		}
+		else if(i == 3){
+			e = new Enemy(0, 0, 10, 15, EnemyType.FAST, 3, 1);
+		}
+		else{
+			e = new Enemy(0, 0, 5, 30, EnemyType.HEAVY, 8, 6);
+		}
+		e.load("Enemy Sprites.png");
+		e.render(g);
 	}
 	
 	public void initCombat(Player p, Enemy e){
@@ -26,13 +45,13 @@ public class Combat {
 		do{
 			if(choice == true){
 			//player attacks
-			//recieveDam(2, p.a);
+				//recieveDam(2, p.a);
 				recieveDam(2, p.attack());
 				checkIsDead(p, e);
 			}
 			else{
 				//enemy attacks
-				//recieveDam(2, p.a);
+				//recieveDam(1, e.a);
 				recieveDam(1, e.attack());
 				checkIsDead(p, e);
 			}
